32eab3e5107ac5c3ee6ded5235001d7014323cc5
Mridhul <mridhul67@gmail.com>
2024-09-15 00:20:25 +0530

    Set up a submit button

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import "./style.css";
 import "./modern-normalize.css";
+
+const submitBtn = document.querySelector(".submit-btn");
+
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+});
