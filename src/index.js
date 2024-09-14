5cf18e31a02ca6b394b294cca4ed3c67fa857f44
Mridhul <mridhul67@gmail.com>
2024-09-15 01:41:48 +0530

    Implement validation on email

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import "./style.css";
 import "./modern-normalize.css";
+import { showError } from "./validation";
 
 const form = document.querySelector(".form");
+const emailField = document.querySelector("#email");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 });
+
+emailField.addEventListener("input", showError);
---
+++ src/validation.js
@@ -0,0 +1,20 @@
+export { showError };
+
+const errorMsgElement = document.querySelector(".error-msg");
+const emailField = document.querySelector("#email");
+
+function showEmailError() {
+  if (emailField.validity.typeMismatch) {
+    errorMsgElement.textContent =
+      "Please enter a valid email ID so we can contact you in cases where you teleport somewhere...unexpected.";
+  } else if (emailField.validity.valueMissing) {
+    errorMsgElement.textContent =
+      "Please enter a valid email ID so we can contact you in cases where you teleport somewhere...unexpected.";
+  } else {
+    errorMsgElement.textContent = "";
+  }
+}
+
+function showError() {
+  showEmailError();
+}
